e2d3cccf079541eafed76e72cd0d7ece48453a54
Gunnar Babicz <gunnarbabicz@yahoo.com>
2023-02-05 23:44:27 -0700

    Finished Exercise 4. Need to properly comment and format.

---
+++ Homework/Advanced OOP/Exercise 4/GunnarBabicz2263Pj4/GunnarBabicz2263Pj4/FileManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GunnarBabicz2263Pj4
+{
+    class FileManager
+    {
+        private string outputDirectory;
+        private string inputContents;
+        private string outputContents;
+
+        public string OutputDirectory 
+        {
+            get { return outputDirectory; }
+            set { outputDirectory = value; }
+        }
+
+        public string InputContents 
+        {
+            get { return inputContents; }
+            set { inputContents = value; }
+        }
+
+        public string OutputContents
+        {
+            get { return outputContents; }
+            set { outputContents = value; }
+        }
+
+        public void OpenFile() 
+        {
+            
+        }
+
+
+
+        public void GetSaveDirectory() 
+        {
+            SaveFileDialog saveFile= new SaveFileDialog();
+            saveFile.Title = "Save File";
+            saveFile.Filter = "Text File (*.txt)|*.txt";
+            saveFile.ShowDialog();
+
+            outputDirectory = saveFile.FileName;
+
+        }
+
+
+        public void SaveEncodedFile(string fileDirectory) 
+        {
+            Rot13DecoderRing Rot13 = new Rot13DecoderRing();
+
+            // file contents saved to a string
+            inputContents = File.ReadAllText(fileDirectory);
+            
+            // file is encoded
+            Rot13.Encode(inputContents);
+            
+            // encoded string defined
+            outputContents = Rot13.EncodedText;
+            
+            GetSaveDirectory();
+
+            if (outputDirectory != "") 
+            { // if the save directory is empty, does not attempt to save file
+                File.WriteAllText(outputDirectory, outputContents);
+            }
+
+
+
+        }
+
+
+
+    }
+}
