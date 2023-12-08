6af6babf2ee2b3a15c1fac58a66698e25fb16176
Gunnar Babicz <gunnarbabicz@yahoo.com>
2023-12-07 20:12:53 -0700

    Removed unnecessary files

--- Homework/Advanced OOP/Project6VSCode/QueueClass.cs
+++
@@ -1,8 +0,0 @@
-namespace Project6
-{
-    internal class QueueClass<T> : DequeueClass<T>
-    {
-        public void push(T value) {pushLeft(value); }
-        public DoubleLinkedListNode<T> pop() {return popRight();}
-    }
-}
--- Homework/Advanced OOP/Project6VSCode/StackClass.cs
+++
@@ -1,8 +0,0 @@
-namespace Project6
-{
-    internal class StackClass<T> : DequeueClass<T>
-    {
-        public void push(T value) {pushLeft(value); }
-        public DoubleLinkedListNode<T> pop() {return popLeft();}
-    }
-}
